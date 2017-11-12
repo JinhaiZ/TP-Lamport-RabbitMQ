6dc10f721da6485ec28802692999993460582b4e
JinhaiZ <zhoujinhaixd@gmail.com>
2017-11-12 17:21:04 +0100

    add example for sharing object between process

--- mutiprocessing_example.py
+++ mutiprocessing_example.py
@@ -1,27 +1,62 @@
-from multiprocessing import Process
-import os
-import time
+from multiprocessing import Process, Condition, Lock  
+from multiprocessing.managers import BaseManager  
+import time, os  
 
-def info(title):
-    print title
-    print 'module name:', __name__
-    if hasattr(os, 'getppid'):  # only available on Unix
-        print 'parent process:', os.getppid()
-    print 'process id:', os.getpid()
+lock = Lock()  
+waitC = Condition(lock)  
+waitP = Condition(lock)  
 
-def f(name):
-    info('function f')
-    print 'hello', name
-    while(True):
-        print("listen msg")
-        time.sleep(1)
+class numeri(object):  
+    def __init__(self):  
+        self.nl = []  
 
-if __name__ == '__main__':
-    info('main line')
-    p = Process(target=f, args=('consumer',))
-    p.start()
-    
-    while(True):
-        print("send msg")
-        time.sleep(3)
-    p.join()
+    def getLen(self):  
+        return len(self.nl)  
+
+    def stampa(self):  
+        print self.nl  
+
+    def appendi(self, x):  
+        self.nl.append(x)  
+
+    def svuota(self):  
+        for i in range(len(self.nl)):  
+            del self.nl[0]  
+
+class numManager(BaseManager):  
+    pass  
+
+numManager.register('numeri', numeri, exposed = ['getLen', 'appendi', 'svuota', 'stampa'])  
+
+def consume(waitC, waitP, listaNumeri):  
+    lock.acquire()  
+    if (listaNumeri.getLen() == 0):  
+        waitC.wait()  
+    listaNumeri.stampa()  
+    listaNumeri.svuota()  
+    waitP.notify()  
+    lock.release()  
+
+def produce(waitC, waitP, listaNumeri):  
+    lock.acquire()  
+    if (listaNumeri.getLen() > 0):  
+        waitP.wait()  
+    for i in range(10):  
+        listaNumeri.appendi(i)  
+    waitC.notify()  
+    lock.release()  
+
+
+def main():  
+    mymanager = numManager()  
+    mymanager.start()  
+    listaNumeri = mymanager.numeri()  
+    producer = Process(target = produce, args =(waitC, waitP, listaNumeri,))  
+    producer.start()  
+    time.sleep(2)  
+    consumer = Process(target = consume, args =(waitC, waitP, listaNumeri,))  
+    consumer.start() 
+    consumer.join()
+    producer.join()
+
+main()
--- site.py
+++ site.py
@@ -12,24 +12,36 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
+class Site(object):
+    
+    def __init__(self, site_id, site_number):
+        self._its_queue_name = "Q{!s}".format(site_id)
+        self._its_exchange_name = "X{!s}".format(site_id)
+        exchange_names = []
+        for i in range(1, site_number+1):
+            if i != site_id:
+                exchange_names.append("X{!s}".format(i))
+        self._binding_exchange_names = exchange_names
+        print(self._its_queue_name, self._its_exchange_name,self._binding_exchange_names)
 
-def start_consumer(queue_name, exchange_names):
-    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
-    example = ExampleConsumer(queue_name, exchange_names)
-    try:
-        example.run()
-    except KeyboardInterrupt:
-        example.stop()
+    def start_consumer(self):
+        logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+        example = ExampleConsumer(self._its_queue_name, self._binding_exchange_names)
+        try:
+            example.run()
+        except KeyboardInterrupt:
+            example.stop()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("usage: python site.py its_queue_name binding_exchange_name1,binding_exchange_name2 its_exchange_name")
+    if len(sys.argv) != 3:
+        print("usage: python site.py its_id total_count_of_sites")
     else:
-        p = Process(target=start_consumer, args=(sys.argv[1], sys.argv[2].split(',')))
+        site = Site(int(sys.argv[1]), int(sys.argv[2]))
+        p = Process(target=site.start_consumer, args=())
         p.start()
-        queue_name = sys.argv[2]
-        exchange_name = sys.argv[3]
-        pub = Publisher(exchange_name, queue_name)
+        # queue_name = sys.argv[2]
+        # exchange_name = sys.argv[3]
+        # pub = Publisher(exchange_name, queue_name)
 
         p.join()
